fe82e9308385a493c4c62035b1054ac4521a5e96
moriaso <muriasosalaon@gmail.com>
2025-04-21 09:47:21 +0300

    hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,22 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-{/* write an <About> component here */}
-
-
 function App() {
   return (
     <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>
+
+      <section id="home">
+        <h1>Welcome Home</h1>
+      </section>
+
+      <section id="about">
+        <h2>About Us</h2>
+      </section>
     </div>
   );
 }
